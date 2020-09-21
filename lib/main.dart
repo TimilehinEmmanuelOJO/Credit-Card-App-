810c03b35e90e90b2687fd895fef48e496a01da8
timi ojo <timilehin175@gmail.com>
2020-09-21 23:05:37 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,11 @@ class MyApp extends StatelessWidget {
         backgroundColor: Color.fromRGBO(228, 233, 247, 100),
         body: SafeArea(
           child: Container(width:400, height:800, color: Color.fromRGBO(228, 233, 247, 100),
+            child: Column(children: <Widget>[SizedBox(height: 40,),Row(children: <Widget>[SizedBox(width: 40,),
+              Container(width: 50, height: 50,child: Image(image: AssetImage("image/rim.JPG")),
+                decoration:BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20))) ,)
+            ],)],),
           ),
         ),
       ),
